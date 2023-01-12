9a4c48e7104238dd317c0347da0b945c3d704fbb
guavalines <konstrictor@gmail.com>
2023-01-11 21:19:15 -0800

    created changeDirection function

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHeight = 20
+const ballDiameter = 20
 const boardWidth = 560
+const boardHeight = 300
+let timerId
+let xDirection = 2
+let yDirection = 2
 
 const userStart = [230, 10]
 let currentPosition = userStart
@@ -99,7 +104,40 @@ grid.appendChild(ball)
 
 // move ball
 function moveBall() {
-  ballCurrentPosition[0] += 2
-  ballCurrentPosition[1] += 2
+  ballCurrentPosition[0] += xDirection
+  ballCurrentPosition[1] += yDirection
   drawBall()
+  checkForCollisions()
+}
+
+timerId = setInterval(moveBall, 30)
+
+// check for collisions
+function checkForCollisions() {
+  // check for wall collisions
+  if (
+      ballCurrentPosition[0] >= (boardWidth - ballDiameter) ||
+      ballCurrentPosition[1] >= (boardHeight - ballDiameter)
+      ) {
+      changeDirection()
+  }
+}
+
+function changeDirection() {
+  if (xDirection === 2 && yDirection === 2) {
+      yDirection = -2
+      return
+  }
+  if (xDirection === 2 && yDirection === -2) {
+      xDirection = -2
+      return
+  }
+  if (xDirection === -2 && yDirection === -2) {
+      yDirection = 2
+      return
+  }
+  if (xDirection === -2 && yDirection === 2) {
+      xDirection = 2
+      return
+  }
 }
